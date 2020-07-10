1584fa89365f97ea5d24c696afcfc942c539c847
martinsaied <martinsaied@gmail.com>
2020-07-09 22:26:08 -0300

    Mas ejercicios

--- guia_4/python_test_env/ejercicio_1/__init__.py
+++ guia_4/python_test_env/ejercicio_1/__init__.py
@@ -15,3 +15,36 @@ def sumaLog(vector):
     return sumaLog(B) + sumaLog(C)
 
 #Si en la posicion I 
+
+# [-2,-1,3,7,8,20,24,25,72,82,90]
+
+def indexEqVal(A, inicio, fin):
+    if(inicio + 1 == fin):
+        return A[inicio] == inicio
+    else:
+        med = inicio + ((fin - inicio) // 2)
+        if (A[med] == med):
+            return True
+        elif (A[med] < med):
+            return indexEqVal(A, med, fin)
+        else:
+            return indexEqVal(A, inicio, med)
+
+def potencia(a, b):
+    numbers = []
+    for x in range(b):
+        numbers.append(a)  
+    
+    return potenciadyc(numbers)
+
+def potenciadyc(numbers):
+    if (len(numbers) == 1):
+        return numbers[0]
+
+    B = numbers[0:len(numbers)//2]
+    C = numbers[len(numbers)//2:]
+    return potenciadyc(B) * potenciadyc(C)
+    
+        
+        
+    
--- guia_4/python_test_env/test.py
+++ guia_4/python_test_env/test.py
@@ -1,19 +1,27 @@
 import unittest
 
 from ejercicio_1 import masIzquierdo
+from ejercicio_1 import indexEqVal
+from ejercicio_1 import potencia
 
 
 class TestSum(unittest.TestCase):
     def test_ejercicio_1(self):
-        """
-        Test that it can sum a list of integers
-        """
         data = [8, 6, 7, 4, 5, 1, 3, 2]
         result = masIzquierdo(data)
         self.assertTrue(result)
         data = [8, 4, 7, 6, 5, 1, 3, 2]
         result = masIzquierdo(data)
         self.assertFalse(result) 
+    
+    def test_ejercicio_2(self):
+        data = [-2,-1,2,7,8,20,24,25,72,82,90]
+        result = indexEqVal(data, 0, len(data))
+        self.assertTrue(result)
+
+    def test_ejercicio_3(self):
+        result = potencia(2, 5)
+        self.assertEqual(result, 32)
 
 if __name__ == '__main__':
     unittest.main()
